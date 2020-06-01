1dd3617853d7232cd70d8e9c9719763af20d211b
Florian Muller <fmuller@student.42.fr>
2020-06-01 23:07:06 +0200

    ex01

---
+++ ex01/YoungestFellah.py
@@ -0,0 +1,20 @@
+# I M P O R T A N T: Execute from sub directory with `python -m ex01.YoungestFellah`
+import pandas as pd
+from ex00.FileLoader import FileLoader
+
+
+def youngestFellah(df, year):
+    return {
+        "f": df.loc[(df["Year"] == year) & (df["Sex"] == "F")]["Age"].min(),
+        "m": df.loc[(df["Year"] == year) & (df["Sex"] == "M")]["Age"].min()
+    }
+
+
+if __name__ == "__main__":
+    data = FileLoader().load("./data/athlete_events.csv")
+
+    print("1896", youngestFellah(data, 1896))
+    print("2004", youngestFellah(data, 2004))
+    print("2016", youngestFellah(data, 2016))
+
+    pass
